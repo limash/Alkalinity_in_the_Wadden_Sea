ae7d1f86e78660da24d555647480bd294f35f6bb
yakubov shamil <yakubov.sha@gmail.com>
2019-12-27 08:24:47 +0100

    Initial version of the manuscript in ipynb

--- utilities.py
+++ utilities.py
@@ -1,17 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.3'
-#       jupytext_version: 0.8.6
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
 import numpy as np
 from math import sqrt, fabs
 
