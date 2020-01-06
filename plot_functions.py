75d8660c8359580de161485bfc2da09badd179ed
yakubov shamil <yakubov.sha@gmail.com>
2020-01-06 23:47:49 +0100

    continue implementing a reasoning for a biogeochemical model construction

--- plot_functions.py
+++ plot_functions.py
@@ -11,6 +11,8 @@ def plot_range(xlabel, ylabel, title, x, values):
     plt.title(title); plt.xlabel(xlabel); plt.ylabel(ylabel);
 
 def plot_year_multi(*args):
+    """*args should be iterateble with 2 elements (value, label);
+       value should be an array of 365 elements"""
     
     fig = plt.figure(figsize=(8, 3))
     ax = fig.add_subplot(1, 1, 1)
@@ -19,35 +21,6 @@ def plot_year_multi(*args):
         ax.plot(ox, arg[0], linewidth=2, label=arg[1])
         
     ax.set_ylabel(r'$values$'); ax.set_xlabel(r'$days$'); ax.legend(loc='best')
-    
-#def plot_year(xlabel, ylabel, title, values):
-#    """values should be an array of 365 values"""
-#
-#    ox = np.arange(1,366,1)
-#    plt.plot(ox, values, 'r-', linewidth=2)
-#    plt.gcf().set_size_inches(8,2)
-#    plt.title(title); plt.xlabel(xlabel); plt.ylabel(ylabel)
-
-#def plot_triple(first, labelfirst, second, labelsecond, third, labelthird):
-#    
-#    # --- get an empty Figure and add an Axes
-#    fig = plt.figure(figsize=(10, 4))
-#    ax = fig.add_subplot(1, 1, 1) # row-col-num
-#    # --- line plot data on the Axes
-#    ox = np.arange(1,366,1)
-#    ax.plot(ox, first, 'b-', linewidth=2, label=labelfirst)
-#    ax.plot(ox, second, 'g-', linewidth=2, label=labelsecond)
-#    ax.plot(ox, third, 'r-', linewidth=2, label=labelthird)
-#    # --- add title and axis labels
-#    #ax.set_title('The name')
-#    ax.set_ylabel(r'$values$', fontsize=16)
-#    ax.set_xlabel(r'$days$', fontsize=16)
-#    # --- plot a legend in the best location
-#    ax.legend(loc='best')
-#    # --- add grid – not in default classic style
-#    ax.grid(True)
-#    # --- improve the layout
-#    fig.tight_layout(pad=1)
     
 if __name__ == '__main__':
     print('This is a plot functions module')
